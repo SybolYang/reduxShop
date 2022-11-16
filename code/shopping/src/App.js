d64a22b35608201cef9b59e82a9109b5d2f2572a
SybolYang <sybolyang@qq.com>
2022-11-16 23:55:57 +0800

    test1

--- code/shopping/src/App.js
+++ code/shopping/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <span>test</span>
+        <span>test1111</span>
         <a
           className="App-link"
           href="https://reactjs.org"
