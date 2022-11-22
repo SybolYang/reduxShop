50f2c15bcf48239794b9a96cc42890625d21f88e
SybolYang <sybolyang@qq.com>
2022-11-22 17:21:49 +0800

    商城項目靜態代碼

---
+++ code/shopping/src/component/App.js
@@ -0,0 +1,13 @@
+import Cart from './cart';
+import Product from './product';
+
+function App() {
+    return (
+        <div>
+            <Cart />
+            <Product />
+        </div>
+    );
+}
+
+export default App;
